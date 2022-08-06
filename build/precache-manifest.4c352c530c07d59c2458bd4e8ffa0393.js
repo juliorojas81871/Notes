75fb8ab0258e369b2adf73cae5b2d288237fc785
Julio Rojas <juliorojas81871@gmail.com>
2022-08-06 19:25:11 -0400

    first commit

---
+++ build/precache-manifest.4c352c530c07d59c2458bd4e8ffa0393.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34a0ed2454c790f9259f45474c8b2717",
+    "url": "/Notes/index.html"
+  },
+  {
+    "revision": "f5115e9113d34e11d7de",
+    "url": "/Notes/static/js/2.88e6856e.chunk.js"
+  },
+  {
+    "revision": "31860cbb50a1e6b81e23",
+    "url": "/Notes/static/js/main.fe6d028b.chunk.js"
+  },
+  {
+    "revision": "61f5ceb7243e93c5543e",
+    "url": "/Notes/static/js/runtime-main.fcbe2c3a.js"
+  }
+]);
