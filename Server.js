7f3dafdfbc4040193e4bf8a94c22af3f627ef863
Julio Rojas <juliorojas81871@gmail.com>
2022-08-16 21:52:25 -0400

    heroku

--- Server.js
+++ Server.js
@@ -5,15 +5,15 @@ require("dotenv").config();
 
 const cors = require("cors");
 
-const routes = require("./routes/TodoRoute");
+const routes = require("./routes/NotesRoute");
 
 const app = express();
 
-//only work on local computer
-const isDev = process.env.NODE_ENV === "development";
-
 app.use(express.json());
 app.use(cors());
+
+//only work on local computer
+const isDev = process.env.NODE_ENV === "development";
 
 // Routes
 app.use(routes);
@@ -33,9 +33,11 @@ mongoose
     .then(() => console.log("Mongodb Connected..."))
     .catch((err) => console.error(err));
 
-const PORT = process.env.PORT || 5000;
+let port = process.env.PORT;
+if(port == null || port == ""){
+    port = 5000;
+}
 
-app.listen(PORT, () => {
+app.listen(port, () => {
     console.log("app listening on port 5000")
 });
-
